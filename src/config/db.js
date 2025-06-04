dba0c4489243636e14d5b738c6a5314c48eb37a0
ramli fariji zamhuri <ramlif828@gmail.com>
2025-06-04 15:53:02 +0700

    third commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Memuat variabel lingkungan
+require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        // Menggunakan URI dari variabel lingkungan MONGO_URI
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            // Opsi ini sudah tidak diperlukan di Mongoose versi 6 ke atas,
-            // tetapi tidak ada salahnya untuk tetap ada.
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-        // Keluar dari proses dengan kode kegagalan jika koneksi database gagal
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Database connection error: ${err.message}`);
+    process.exit(1); // Exit with failure
+  }
 };
 
 module.exports = connectDB;
