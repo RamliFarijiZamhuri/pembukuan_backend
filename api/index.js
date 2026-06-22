545a65a7d53cac60610b118e0632fdb9638e0e9c
Ramli Fariji Zamhuri <ramlif828@gmail.com>
2026-06-23 01:18:44 +0700

    Fix: Remove serverless-http wrapper for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-const serverless = require('serverless-http');
 const app = require('../src/app');
 
-// Ekspor aplikasi Express yang dibungkus sebagai fungsi serverless untuk Vercel
-module.exports = serverless(app);
+// Ekspor aplikasi Express untuk Vercel
+module.exports = app;
